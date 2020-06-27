bc3f03e9d5f191f370f72d42b93ff85d8f4c8da1
anoumana <anou.mana@gmail.com>
2020-06-27 15:23:43 -0600

    search cahce fix

--- src/campaignMgmt/reducers/campaignListReducer.js
+++ src/campaignMgmt/reducers/campaignListReducer.js
@@ -1,28 +1,21 @@
-import {CAMPAIGN_LIST_SUCCESS, CAMPAIGN_LIST_FAIL, CAMPAIGN_SEARCH_SUCCESS} from '../types';
+import {CAMPAIGN_LIST_SUCCESS, CAMPAIGN_LIST_FAIL, CAMPAIGN_SEARCH_SUCCESS, CAMPAIGN_SEARCH_INIT} from '../types';
 import { act } from 'react-test-renderer';
 
 const INITIAL_STATE= { campaignListInfo: [] };
-// const RESET_STATE= { campaignForm: {
-//     campaignName: '',
-//     campaignDesc: '',
-//     capaignMobile: '',
-//     categoryName:'',
-//     campaignDiscount: '',
-//     campaignKey: ''
-
-// } };
+const SEARCH_INITIAL_STATE= { campaignSearchList: [] };
 
 export default (state = INITIAL_STATE, action) => {
 //    console.log("list reducer:", action.payload);
     
     switch (action.type) {
+        case CAMPAIGN_SEARCH_INIT:
+            console.log("in CAMPAIGN_SEARCH_INIT:", action.payload)
+            return {...state, ...SEARCH_INITIAL_STATE   };
         case CAMPAIGN_SEARCH_SUCCESS:
-            console.log("in CAMPAIGN_SEARCH_SUCCESS:", action.payload)
-            //console.log("CAMPAIGN_SEARCH_SUCCESS");
+//            console.log("in CAMPAIGN_SEARCH_SUCCESS:", action.payload)
             return {...state, campaignSearchList:action.payload   };
         case CAMPAIGN_LIST_SUCCESS:
-            console.log("in CAMPAIGN_LIST_SUCCESS:", action.payload)
-            //console.log("CAMPAIGN_LIST_SUCCESS");
+//            console.log("in CAMPAIGN_LIST_SUCCESS:", action.payload)
             return {...state, campaignList:action.payload   };
         case CAMPAIGN_LIST_FAIL:
 //            console.log("in CAMPAIGN_LIST_FAIL:", action.payload)
